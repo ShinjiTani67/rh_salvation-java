e01a4516bb83db95b6bcfe0051bb0baee814e3ad
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-11-10 11:31:58 -0300

    update employee dto

--- src/main/java/com/br/rhsalvation/fiap/demo/dto/EmployeeDTO.java
+++ src/main/java/com/br/rhsalvation/fiap/demo/dto/EmployeeDTO.java
@@ -1,4 +1,29 @@
 package com.br.rhsalvation.fiap.demo.dto;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class EmployeeDTO {
+
+    private UUID uuid;
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+
 }
