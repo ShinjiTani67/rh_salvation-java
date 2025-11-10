1a1c703c9374b07151961c1a12db66a640c04465
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-11-10 11:19:56 -0300

    git intlzr

---
+++ src/main/java/com/br/rhsalvation/fiap/demo/RhSalvationApplication.java
@@ -0,0 +1,13 @@
+package com.br.rhsalvation.fiap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhSalvationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhSalvationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/rhsalvation/fiap/demo/RhSalvationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rhsalvation.fiap.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhSalvationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
