8130c9017135525efa96d283e4bb0a437a1f3c26
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-11-10 11:33:24 -0300

    update eployee controller

--- src/main/java/com/br/rhsalvation/fiap/demo/controller/EmployeeController.java
+++ src/main/java/com/br/rhsalvation/fiap/demo/controller/EmployeeController.java
@@ -1,4 +1,15 @@
 package com.br.rhsalvation.fiap.demo.controller;
 
+
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+@AllArgsConstructor
+@Log
 public class EmployeeController {
+    //fazer controller
 }
