c69a5cd65c0d3dd95dc8ad7d70a9c9416ac2cf84
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-11-11 15:42:08 -0300

    create rhdto

---
+++ src/main/java/com/br/rhsalvation/fiap/demo/dto/RhDTO.java
@@ -0,0 +1,30 @@
+package com.br.rhsalvation.fiap.demo.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RhDTO {
+    
+    private UUID uuid;
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+
+
+}
