49b94c59cd44c130acfcde0c40e026bd1df46fe8
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-11-13 16:06:13 -0300

    update employeerepository

--- src/main/java/com/br/rhsalvation/fiap/demo/repository/EmployeeRepository.java
+++ src/main/java/com/br/rhsalvation/fiap/demo/repository/EmployeeRepository.java
@@ -3,7 +3,14 @@ package com.br.rhsalvation.fiap.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.br.rhsalvation.fiap.demo.entity.Employee;
 
+import java.util.Optional;
+import java.util.UUID;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Long>{
+
+
+public interface EmployeeRepository extends JpaRepository<Employee,UUID>{
+
+    Optional<Employee> findByUuid(UUID uuid);
+    Optional<Employee> findByEmail(String email);
 
 }
