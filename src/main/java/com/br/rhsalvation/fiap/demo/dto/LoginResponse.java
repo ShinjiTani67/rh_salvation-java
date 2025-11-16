14687c8f45347358e5ba879a05bb39bd2b30623a
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-11-16 11:50:13 -0300

    create loginresponse

---
+++ src/main/java/com/br/rhsalvation/fiap/demo/dto/LoginResponse.java
@@ -0,0 +1,10 @@
+package com.br.rhsalvation.fiap.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+}
